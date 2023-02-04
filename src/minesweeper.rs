8e73a6e74cf4b049dc27fda04f80912e8e4230b3
vitosotdihaet <vitostikitos@gmail.com>
2023-02-04 16:19:35 +0300

    Implement Display for Cell

--- src/minesweeper.rs
+++ src/minesweeper.rs
@@ -1,10 +1,10 @@
 use rand::seq::SliceRandom;
 use std::{
     cmp::{min, max},
-    fmt,
+    fmt
 };
 
-const MINE_COUNT: &[usize] = &[0, 1, 2, 3, 4, 5, 6, 7, 8];
+const MINE_COUNT_DISPLAY: &[char] = &['0','1','2','3','4','5','6','7','8'];
 
 #[derive(Clone, Copy, Default)]
 pub struct Cell {
@@ -12,6 +12,20 @@ pub struct Cell {
     pub mine: bool,
     pub flag: bool,
     pub revealed: bool,
+}
+
+impl fmt::Display for Cell {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let mut result = '?';
+        if self.flag {
+            result = '!';
+        } else if self.revealed && self.mine {
+            result = 'M';
+        } else if self.revealed {
+            result = MINE_COUNT_DISPLAY[self.surrounds as usize];
+        }
+        write!(f, "{}", result)
+    }
 }
 
 pub struct Minesweeper {
@@ -34,7 +48,7 @@ impl Default for Minesweeper {
 
 impl fmt::Display for Minesweeper {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let mut fmt = String::new();
+        let mut result = String::new();
         let w_length = self.width.to_string().len();
         let h_length = self.height.to_string().len();
 
@@ -42,28 +56,21 @@ impl fmt::Display for Minesweeper {
         for n in 0..self.width { 
             to_print += &(" ".repeat(w_length - (n + 1).to_string().len() + 1) + &(n + 1).to_string());
         }
-        fmt.push_str(&to_print);
-        fmt.push('\n');
-        fmt.push_str(&" ".repeat(h_length + 1));
-        fmt.push_str(&"_".repeat(to_print.len() - h_length - 1));
-        fmt.push('\n');
+        result.push_str(&to_print);
+        result.push('\n');
+        result.push_str(&" ".repeat(h_length + 1));
+        result.push_str(&"_".repeat(to_print.len() - h_length - 1));
+        result.push('\n');
 
         for y in 0..self.height {
-            fmt.push_str(&((y + 1).to_string() + &" ".repeat(h_length - (y + 1).to_string().len() + 1) + "|"));
-
+            result.push_str(&((y + 1).to_string() + &" ".repeat(h_length - (y + 1).to_string().len() + 1) + "|"));
             for x in 0..self.width {
-                if self.grid[y][x].flag {
-                    fmt.push_str("🚩");
-                } else if self.grid[y][x].revealed {
-                    fmt.push_str(&MINE_COUNT[self.grid[y][x].surrounds as usize].to_string());
-                } else {
-                    fmt.push_str("?");
-                }
-                fmt.push_str(&" ".repeat(w_length));
+                result.push_str(&format!("{}", self.grid[y][x]));
+                result.push_str(&" ".repeat(w_length));
             }
-            fmt.push('\n');
+            result.push('\n');
         }
-        write!(f, "{}", fmt)
+        write!(f, "{}", result)
     }
 }
 
@@ -138,21 +145,31 @@ impl Minesweeper {
         }
         self.grid[y][x].revealed = true;
 
-        for cx in max(0, x as isize - 1) as usize..=min(self.width - 1, x + 1) {
-            for cy in max(0, y as isize - 1) as usize..=min(self.height - 1, y + 1) {
-                if !(cx == x && cy == y) {
-                    self.open_empty(cx, cy);
-                }
+        let xrange = max(0, x as isize - 1) as usize..=min(self.width - 1, x + 1);
+        let yrange = max(0, y as isize - 1) as usize..=min(self.height - 1, y + 1);
+
+        for cy in yrange {
+            for cx in xrange.clone() {
+                self.open_empty(cx, cy);
             }
         }
     }
 
     fn generate_grid(&mut self, x: usize, y: usize) {
+        if self.number_of_mines == self.width * self.height {
+            for y in 0..self.height  {
+                for x in 0..self.width {
+                    self.grid[y][x].mine = true;
+                }
+            }
+            return;
+        }
+
         let mut rng = rand::thread_rng();
         let mut mines: Vec<(usize, usize)> = vec![];
-        for cx in 0..self.width {
-            for cy in 0..self.height {
-                if !(cx == x && cy == y) {
+        for cy in 0..self.height {
+            for cx in 0..self.width {
+                if !(cy == y && cx == x) {
                     mines.push((cx, cy));
                 }
             }
@@ -162,9 +179,12 @@ impl Minesweeper {
             let (x, y) = (*cx, *cy);
             self.grid[y][x].mine = true;
 
-            for cx in max(0, x as isize - 1) as usize..=min(self.width - 1, x + 1) {
-                for cy in max(0, y as isize - 1) as usize..=min(self.height - 1, y + 1) {
-                    if !(cx == x && cy == y) {
+            let xrange = max(0, x as isize - 1) as usize..=min(self.width - 1, x + 1);
+            let yrange = max(0, y as isize - 1) as usize..=min(self.height - 1, y + 1);
+            
+            for cy in yrange {
+                for cx in xrange.clone() {
+                    if !(cy == y && cx == x) {
                         self.grid[cy][cx].surrounds += 1;
                     }
                 }
